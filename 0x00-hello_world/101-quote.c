a71581973d72ff9697c7961e238fa9e4c5089985
Anas Khalid <anas.k7aleed@gmail.com>
2023-09-07 23:34:20 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
-/*
- * main - entry
- * desc :simple opreating system
- */
-
+/**
+* main - Entry point
+*
+* Descripiton : some of unix
+*
+* Return: Alawys 0 (success)
+*/
 int main(void)
 {
-	char quo = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1.quo.59);
 	return (1);
