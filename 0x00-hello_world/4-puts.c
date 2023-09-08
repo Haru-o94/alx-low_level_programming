0c954209448584a846fca16c87ec4ee470a94847
Anas Khalid <anas.k7aleed@gmail.com>
2023-09-07 23:11:42 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
- *main -  entry
- *desc: some desc
- */
+* main - Entry point
+*
+* Descripiton : c porgram usuing puts
+*
+* Return: Alawys 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
