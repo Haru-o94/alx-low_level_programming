ae5eaf7ab9f13c778cd00952a9ebf62b608d0ab4
Anas Khalid <anas.k7aleed@gmail.com>
2023-09-12 16:02:26 +0200

    comment

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - print sign of number
+* @c: check of the input
+*
+* Return: 1 if c + or 0 if zero or -1 less than 0
+*/
+
+int _isalpha(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+}
