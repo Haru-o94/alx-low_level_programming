18cd5b1f3030edb771f299c1bb3143ef9851784b
Anas Khalid <anas.k7aleed@gmail.com>
2023-09-12 14:49:05 +0200

    comment

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description : print_alpha - utilizes on the _putchar
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+return (0);
+}
