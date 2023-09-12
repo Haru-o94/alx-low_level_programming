9124811bea0639a33c81461c8c4256b90a10447d
Anas Khalid <anas.k7aleed@gmail.com>
2023-09-12 16:43:25 +0200

    comment

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - sum two number
+* @a: check of the frist
+* @b: check of the secend
+*
+* Return: the sum
+*/
+
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
