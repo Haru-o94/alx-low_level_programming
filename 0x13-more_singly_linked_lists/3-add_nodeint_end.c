98061a63a95bf4dc672c22f33ffc1c3997f3e835
Haru-o94 <anas.k7aleed@gmail.com>
2023-10-23 14:25:19 +0200

    edit betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
-/** * add_nodeint - create new node at begin of list.
+/**
+ * add_nodeint_end - create new node at begin of list.
  * @head: point to the head of list
  * @n: num of element
  *
